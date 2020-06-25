77f4ca3b856dda7257311e33a5138f0dca3325b0
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2020-06-24 22:47:43 -0600

    Add project files.

---
+++ WinTestConnectSqlServer/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WinTestConnectSqlServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butConnect
+            // 
+            this.butConnect.Location = new System.Drawing.Point(441, 302);
+            this.butConnect.Name = "butConnect";
+            this.butConnect.Size = new System.Drawing.Size(257, 108);
+            this.butConnect.TabIndex = 0;
+            this.butConnect.Text = "Connect";
+            this.butConnect.UseVisualStyleBackColor = true;
+            this.butConnect.Click += new System.EventHandler(this.butConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.butConnect);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butConnect;
+    }
+}
+
---
+++ WinTestConnectSqlServer/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTestConnectSqlServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connetionString;
+                SqlConnection cnn;
+                // connetionString = @"Data Source=WIN-3VU0IUD67IK;Initial Catalog=Demodb;User ID=sa;Password=demol23";
+                // localhost\sqlexpress
+                connetionString = @"Data Source=WIN-3VU0IUD67IK\SQLEXPRESS;Initial Catalog=Agenda;User ID=sa;Password=Flpvrgs1966";
+                //connetionString = @"Data Source=WIN-3VU0IUD67IK\SQLEXPRESS;Integrated Security = SSPI;Initial Catalog=Agenda;";
+                //Integrated Security = SSPI; Initial Catalog = pubs
+                // connetionString = @"Data Source=tcp:10.25.1.56;Initial Catalog=Agenda;User ID=sa;Password=Flpvrgs1966";
+
+                cnn = new SqlConnection(connetionString);
+                cnn.Open();
+                MessageBox.Show("Connection Open  !");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
